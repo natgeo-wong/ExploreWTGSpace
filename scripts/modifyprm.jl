ae502540c43500a952b1cce961bac8b1bb74811c
Nathanael Wong <natgeo.wong@outlook.com>
2021-09-25 16:17:53 -0400

    Standardizing naming format for consistency

--- scripts/modifyprm.jl
+++ scripts/modifyprm.jl
@@ -2,19 +2,18 @@ using DrWatson
 @quickactivate "ExploreWTGSpace"
 using Printf
 
-exp = "P064km305d0"
+exp = "D0641km300"
 tprm = projectdir("exp","tmp.prm")
 
-for powerii = 0 : 9
-    conii = "damping$(@sprintf("%03d",2^powerii))"
+for powerii = [1,2,4,8,16,32,64,128,256,512]
+    conii = "damping$(@sprintf("%03d",powerii))"
+    mkpath(projectdir("exp","prm",exp,conii))
     for imember = 1 : 10
-        prm = projectdir("exp","prm",exp,conii,"member$(@sprintf("%02d",imember)).prm")
+        prm  = projectdir("exp","prm",exp,conii,"member$(@sprintf("%02d",imember)).prm")
         open(tprm,"w") do fprm
             open(prm,"r") do oprm
                 s = read(oprm,String)
-                s = replace(s,"nstop    = 2160000,"=>"nstop    = 1440000,")
-                s = replace(s,"nprint   = 4320,"=>"nprint   = 2880,")
-                s = replace(s,"nstat    = 4320,"=>"nstat    = 2880,")
+                s = replace(s,"tabs_s     = 301.70,"=>"tabs_s     = 300.,")
                 write(fprm,s)
             end
         end
