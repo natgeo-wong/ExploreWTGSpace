8e27d78afca2efd9ae223d3e663814f1c2bff59b
Documenter.jl <documenter@juliadocs.github.io>
2023-09-06 23:27:11 +0000

    build based on 67d0bc3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#[ExploreWTGSpace](https://github.com/natgeo-wong/ExploreWTGSpace)","page":"Home","title":"ExploreWTGSpace","text":"","category":"section"},{"location":"index.html","page":"Home","title":"Home","text":"Exploring the different implementations and parameters of the WTG Approximation","category":"page"},{"location":"index.html","page":"Home","title":"Home","text":"Text","category":"page"}]
+}
