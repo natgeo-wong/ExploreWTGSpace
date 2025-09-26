7a2fd6d72dc98973aa123ddaf98fddae8c6caf06
Nathanael Wong <natgeo.wong@outlook.com>
2025-09-26 03:48:36 -0400

    Streamline the run

--- scripts/modifysam/runtemplates/modelrun.sh
+++ scripts/modifysam/runtemplates/modelrun.sh
@@ -61,10 +61,7 @@ done
 cd ..
 
 cd $scriptdir
-export OMPI_MCA_btl="self,openib"
-unset I_MPI_PMI_LIBRARY
-export I_MPI_JOB_RESPECT_PROCESS_PLACEMENT=0
-mpirun -np $SLURM_NTASKS $SAMname > ./LOGS/samrun.${SLURM_JOBID}.log
+srun $SAMname > ./LOGS/samrun.${SLURM_JOBID}.log
 
 exitstatus=$?
 echo SAM stopped with exit status $exitstatus
